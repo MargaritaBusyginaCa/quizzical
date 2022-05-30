a705fe6bc74560bb5d55e0dade0415055189a016
MargaritaCa <busy0001@algonquinlive.com>
2022-05-30 18:30:39 -0400

    Add gameStart function that changes state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import { Link, Switch, Route } from "react-router-dom";
+import {useState} from "react"
 import Welcome from './Welcome';
 import Quizz from './Quizz';
 function App() {
+  const [startGame, setStartGame] = useState(false)
+  function gameStart(){
+    setStartGame(true)
+    
+  }
   return (
     <div>
       <Switch>
-         <Route exact path="/"><Welcome/></Route>
+         <Route exact path="/"><Welcome handleClick={gameStart}/></Route>
          <Route path="/quizz"><Quizz/></Route>
        </Switch>
     </div>
--- src/Welcome.js
+++ src/Welcome.js
@@ -1,12 +1,12 @@
 import {Route, Link, Switch} from 'react-router-dom'
 import Quizz from './Quizz'
-function Welcome(){
+function Welcome(props){
   return(
       <div className='welcome--page_container'>
           <div className='lemon-circle'></div>
           <h1>Quizzical</h1>
           <p>Fun way to test your knowledge</p>
-          <Link to="/quizz" className='primary--button'>Start Quizz</Link>
+          <Link to="/quizz" className='primary--button' onClick={props.handleClick}>Start Quizz</Link>
           <div className='blue-circle'></div>
               
       </div>
