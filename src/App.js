82d6da8a0a489f5057007da077afef85b0cbce93
MargaritaCa <busy0001@algonquinlive.com>
2022-05-31 13:28:35 -0400

    add selectAnswer(), calculateScore() functions. At this stage the app displays questions, calculates the score and shows it on the screen

--- src/App.js
+++ src/App.js
@@ -15,28 +15,46 @@ function App() {
   function gameStart(){setStartGame(true)}
   
   useEffect(() => {
+    /*Temporary array for storing data within the API call*/
     let questionsArray = []
-    fetch("https://opentdb.com/api.php?amount=5&difficulty=easy&type=multiple")
-    .then(res => res.json())
-    .then(data => questionsArray = data.results)
-    .then(() => {
-      questionsArray = questionsArray.map(q => {
-        let answersArray = q.incorrect_answers.map(wrongAnswer =>{
+    fetch("https://opentdb.com/api.php?amount=6&difficulty=easy&type=multiple")
+     .then(res => res.json())
+     .then(data => questionsArray = data.results)
+     .then(()=>{
+       /*Rewriting questionsArray so it has a newly returned set of objects with the attributes that we need*/
+       questionsArray = questionsArray.map(q => {
+        /*Because we have a nested array (question -> correct_answer -> incorrect_answers (3 elements) we need to map through the
+        array of incorrenct_answers as well and return it with the attributes that we need*/
+        let answersArray = q.incorrect_answers.map( wrongAnswer => {
           return{id:nanoid(), value:wrongAnswer, isCorrect:false, isSelected:false}
         })
-        answersArray.push({value:q.correct_answer, isCorrect:true, isSelected:false})
-        return{category:q.category, question:q.question, answers: answersArray}
-      })
-      
-    })
-    .then(() => {setQuestions(questionsArray)})
+        /*As the answersArray already contains incorrect answers after mapping, we also need to push a correct answer to it*/
+        answersArray.push({id:nanoid(), value:q.correct_answer, isCorrect:true, isSelected:false})
+        shuffleAnswers(answersArray)
+       /*End questionsArray mapping with returning the object with all the attributes we need*/
+       return {id:nanoid(),category:q.category, question: q.question, answers: answersArray}
+       })
+     })
+     /*End an API call with setting questions state with the new array of objects*/
+     .then(() => setQuestions(questionsArray))
   }, [])
-  console.log(questions)
+
+ function shuffleAnswers(array){
+  let currentIndex = array.length
+  while (currentIndex != 0){
+    let randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex --
+
+    [array[currentIndex], array [randomIndex]] = [array[randomIndex], array[currentIndex]]
+ }
+  return array
+}
+
   return (
     <div>
       <Switch>
          <Route exact path="/"><Welcome handleClick={gameStart}/></Route>
-         <Route path="/quizz"><Quizz/></Route>
+         <Route path="/quizz"><Quizz questions={questions} setQuestions={setQuestions}/></Route>
        </Switch>
     </div>
   );
--- src/Quizz.js
+++ src/Quizz.js
@@ -1,9 +1,53 @@
 import React, {useState, useEffect} from "react"
-function Quizz (props){
-
+import {nanoid} from 'nanoid';
+function Quizz ({questions, setQuestions}){
+    const [count, setCount] = useState(0)
+    function selectAnswer(questionId, answerObject){
+        /*If one of the elements that have different id from the passedf objects has 
+        isSelected:true turn it to false. For the passed object, turn isSelected to true */
+        for(let question of questions){
+            if(question.id == questionId){
+                for (let answer of question.answers){
+                    if(answer.id!=answerObject.id && answer.isSelected == true){
+                        answer.isSelected=false
+                    }
+                    answerObject.isSelected = true
+                    console.log(answer)
+                }
+            }
+            
+        }    
+       
+    }
+    const questionsDisplay = questions.map(q =>{
+        return (
+            <div>
+              <h2>{q.question}</h2>
+              <div>
+                <button onClick={() => selectAnswer(q.id, q.answers[0])}>{q.answers[0].value}</button>
+                <button onClick={() => selectAnswer(q.id, q.answers[1])}>{q.answers[1].value}</button>
+                <button onClick={() => selectAnswer(q.id, q.answers[2])}>{q.answers[2].value}</button>
+                <button onClick={() => selectAnswer(q.id, q.answers[3])}>{q.answers[3].value}</button>
+              </div>
+            </div>
+        )
+    })
+    
+    function calculateScore(){
+        for(let question of questions){
+            for (let answer of question.answers){
+                if(answer.isSelected && answer.isCorrect){
+                    setCount(prevCount => prevCount + 1)
+                }
+            }
+        }
+        
+    }
     return(
         <div>
-            Quizz page
+            {questionsDisplay}
+            <button className="finish--btn" onClick={calculateScore}>Submit Answers</button>
+            <h1>You scored: {count} / 6</h1>
         </div>
     )
 }
